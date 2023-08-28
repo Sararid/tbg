2ab1136897f7e40c2fb5b9522e7dc59a493d2643
saraRid <sara.ridouane@porsche.digital>
2023-08-28 16:50:08 +0200

    update card component

--- app/components/Gift.tsx
+++ app/components/Gift.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react"
 import YouTube from "react-youtube";
-
+import './Gift.scss'
 function Gift() {
   const [display, setDisplay] = useState<string | undefined>("visible")
   console.log("🚀 ~ file: Gift.tsx:5 ~ Gift ~ display:", display)
@@ -20,13 +20,14 @@ function Gift() {
 
   return (
     <div style={{
-      position: "absolute", top: 0, backgroundColor: " rgb(6, 214, 160,1)", zIndex: 99999, width: "300px",
+      position: "absolute", top: 0, 
+zIndex: 99999, width: "300px",
       height: "450px", display: display
-    }}>
+    }} className="giftStyle">
       
-      <button onClick={handleClick} style={{ padding: "20px", width: "20px", top: 0, backgroundColor: " rgb(6, 214, 160,1)", height: "20px", borderColor: "transparent" }}><span role="img" aria-label="dog">⬅</span> </button>
+      <button onClick={handleClick} className="giftStyle"  style={{ margin: "15px", top: 0, color: "white",  borderColor: "transparent" }}><span role="img" aria-label="dog">⬅</span> </button>
      
-      <p>Are you ready to try your first Hammam experience?! <span role="img" aria-label="dog">💆</span> We have a date for the 20th of September, very needed after our long trip, I hope you love it      <span role="img" aria-label="dog">😘</span></p>
+      <p style={{color: "white"}}>Are you ready to try your first Hammam experience?! <span role="img" aria-label="dog">💆</span> We have a date for the 20th of September, very needed after our long trip, I hope you love it      <span role="img" aria-label="dog">😘</span></p>
       <YouTube videoId="wA2s4tDviNM" opts={opts} />
          
     </div>
