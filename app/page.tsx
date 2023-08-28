467b39b606b8c4f6fdb7a32bf6214e7284bb5e0c
saraRid <sara.ridouane@porsche.digital>
2023-08-28 17:01:38 +0200

    remove image

--- app/page.tsx
+++ app/page.tsx
@@ -7,19 +7,20 @@ import Pass from "./pages/Pass";
 import "./App.css";
 
 function Home() {
-  const password = process.env.APP_ENVPASSWORD
+  const password = `${process.env.APP_ENVPASSWORD}`
+  console.log("🚀 ~ file: page.tsx:11 ~ Home ~ password:", password)
 
   const [inputValue, setInputValue] = useState("")
   const handleInput = (ev: any) => {
     setInputValue(ev.target.value)
 
   }
-  console.log("🚀 ~ file: App.tsx:9 ~ App ~ password:", inputValue)
+  console.log("🚀 ~ file: App.tsx:9 ~ A:", inputValue)
   return (
     <div className="App" >
       {/* <Pass onChange={handleInput} value={inputValue} /> */}
-      <Home2 />
-    {/* {inputValue === process.env.HONEY ? <Home2 /> : <Pass onChange={handleInput} value={inputValue} />}  */}
+      {/* <Home2 /> */}
+      {inputValue === "honey" ? <Home2 /> : <Pass onChange={handleInput} value={inputValue} />}  
     </div >
   );
 }
